93de0d9b3774717e6d2fc3ef0f8e3aedf3826d99
jrodriguez <jrodriguez@axt.email>
2024-03-27 15:16:04 +0100

    Including GPIO-control for Tango device off de new pins available

--- tcp_server/rpi_gpio_server.py
+++ tcp_server/rpi_gpio_server.py
@@ -18,7 +18,8 @@ GPIO.setwarnings(False)
 
 class TCP(socketserver.BaseRequestHandler):
 
-    pinlist = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 32, 36, 38, 40]
+    pinlist = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26,
+               29, 31, 32, 33, 35, 36, 38, 37, 40]
 
     def handle(self):
         #self.request.settimeout(5)
